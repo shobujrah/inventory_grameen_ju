65f21e06328f2232d038a7c0d36754211f74ce99
Md.Shobujur Rahman <134444385+shobujrah@users.noreply.github.com>
2025-09-10 12:50:30 +0600

    initialized

---
+++ README.md
@@ -0,0 +1,1 @@
+# GJUS_Inventory
---
+++ app/Models/User.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+# A #
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Traits\HasRoles;
+# A #
+use App\Models\Requisition;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * Set the default guard for this model.
+     *
+     * @var string
+     */
+    protected $guard_name = 'user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'branch_id',
+        'branch_name',
+        'branch_type',
+        'name',
+        'email',
+        'join_date',
+        'phone_number',
+        'is_verified',
+        'status',
+        'position',
+        'role_name',
+        'date_of_birth',
+        'navigate_to',
+        // 'blood_group',
+        'address',
+        'avatar',
+        // 'position',
+        'department',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    # A #
+    public static function getpermissionGroups()
+    {
+        $permission_groups = DB::table('permissions')
+            ->select('group_name as name')
+            ->groupBy('group_name')
+            ->get();
+        return $permission_groups;
+    }
+
+    public static function getpermissionsByGroupName($group_name)
+    {
+        $permissions = DB::table('permissions')
+            ->select('name', 'id')
+            ->where('group_name', $group_name)
+            ->get();
+        return $permissions;
+    }
+
+    public static function roleHasPermissions($role, $permissions)
+    {
+        $hasPermission = true;
+        foreach ($permissions as $permission) {
+            if (!$role->hasPermissionTo($permission->name)) {
+                $hasPermission = false;
+                return $hasPermission;
+            }
+        }
+        return $hasPermission;
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $getUser = self::orderBy('user_id', 'desc')->first();
+
+            if ($getUser) {
+                $latestID = intval(substr($getUser->user_id, 3));
+                $nextID = $latestID + 1;
+            } else {
+                $nextID = 1;
+            }
+            $model->user_id = '000' . sprintf("%03s", $nextID);
+            while (self::where('user_id', $model->user_id)->exists()) {
+                $nextID++;
+                $model->user_id = '000' . sprintf("%03s", $nextID);
+            }
+        });
+    }
+
+    public function requisition()
+    {
+        return $this->hasMany(Requisition::class);
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+
+
+
+
+
+}
